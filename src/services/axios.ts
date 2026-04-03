85367958a5fccc3b7175c04717367aeeabeca1bc
vincent cupid tabiongan <cupidvincent@gmail.com>
2026-04-03 13:43:41 +0800

    fe

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -7,3 +7,36 @@ export const axiosClient = axios.create({
     },
     withCredentials: true,
 });
+
+axiosClient.interceptors.response.use(
+    response => response,
+    async error => {
+        const originalRequest = error.config;
+
+        // ❗ prevent infinite loop
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            console.log('hereeeeeeee');
+            if (
+                originalRequest.url?.includes('/auth/login') ||
+                originalRequest.url?.includes('/auth/refresh')
+            ) {
+                return Promise.reject(error); // don't retry
+            }
+            originalRequest._retry = true;
+
+            try {
+                // 🔁 call refresh endpoint
+                await axiosClient.post('/auth/refresh');
+
+                // ✅ retry original request
+                return axiosClient(originalRequest);
+            } catch (refreshError) {
+                // ❌ refresh failed → logout user
+                console.error('Refresh failed', refreshError);
+                return Promise.reject(refreshError);
+            }
+        }
+
+        return Promise.reject(error);
+    }
+);
